c1263177faf9af1f2675df9bc88c046a643cdda7
miriam schlindwein <schlindwein@hallowelt.com>
2023-03-10 11:24:55 +0100

    Remove unused styling modules
    ERM31248
    
    [4.x]
    
    Change-Id: I85f0ce67db6ee23805568f4e0402a69a808da6b9

--- src/Hook/BeforePageDisplay/AddRessources.php
+++ src/Hook/BeforePageDisplay/AddRessources.php
@@ -5,7 +5,6 @@ namespace BlueSpice\QrCode\Hook\BeforePageDisplay;
 class AddRessources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
-		$this->out->addModuleStyles( 'ext.qrcode.styles' );
 		$this->out->addModules( 'ext.qrcode.dialog' );
 
 		return true;
