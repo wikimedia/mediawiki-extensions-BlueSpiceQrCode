cec1cdbe244a9e8c3803a0af2306e444bf99b2e8
Matviienko <vmatviienko@hallowelt.com>
2026-06-12 17:30:11 +0200

    Handle files using a component
    ERM45736
    [5.3]
    
    Change-Id: Iee118d266e1527bf2f51c4a324113f2bbfc83fc8

--- src/DynamicFileDispatcher/QrCode.php
+++ src/DynamicFileDispatcher/QrCode.php
@@ -6,17 +6,29 @@ use MediaWiki\Permissions\Authority;
 use MediaWiki\Title\TitleFactory;
 use MWStake\MediaWiki\Component\DynamicFileDispatcher\IDynamicFile;
 use MWStake\MediaWiki\Component\DynamicFileDispatcher\IDynamicFileModule;
+use MWStake\MediaWiki\Component\FileStorageUtilities\StorageHandler;
+use RepoGroup;
 
 class QrCode implements IDynamicFileModule {
 
 	/** @var TitleFactory */
 	private $titleFactory;
 
+	/** @var RepoGroup */
+	private $repoGroup;
+
+	/** @var StorageHandler */
+	private $storageHandler;
+
 	/**
 	 * @param TitleFactory $titleFactory
+	 * @param RepoGroup $repoGroup
+	 * @param StorageHandler $storageHandler
 	 */
-	public function __construct( TitleFactory $titleFactory ) {
+	public function __construct( TitleFactory $titleFactory, RepoGroup $repoGroup, StorageHandler $storageHandler ) {
 		$this->titleFactory = $titleFactory;
+		$this->repoGroup = $repoGroup;
+		$this->storageHandler = $storageHandler;
 	}
 
 	public function getFile( array $params ): ?IDynamicFile {
@@ -24,7 +36,9 @@ class QrCode implements IDynamicFileModule {
 			$this->titleFactory,
 			$params['pagename'] ?? $this->titleFactory->newMainPage()->getPrefixedText(),
 			$params['query'] ?? '',
-			isset( $params['size'] ) ? (int)$params['size'] : 100
+			isset( $params['size'] ) ? (int)$params['size'] : 100,
+			$this->repoGroup,
+			$this->storageHandler
 		);
 	}
 
